4f994375543f5db63117d65b168fb4400eaa93d1
猜猜看 <louis664724128@hotmail.com>
2015-03-02 13:37:09 +0800

    上传未同步文件.

---
+++ C_Excel/FunctionsCS.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace C_Excel
+{
+    class FunctionsCS
+    {
+        private Form1.WorkTime _dateTime;
+        public Form1.WorkTime dateTime
+        {
+            get { return _dateTime; }
+            set { this._dateTime = value; }
+        }
+
+
+        public Form1.WorkTime ConvertStringToDateTime(string str)
+        {
+            //Form1.WorkTime dateTime;// = new Form1.WorkTime();
+            List<string> MathGroup = new List<string>();
+            Form1.AMTime AmTime;
+            Form1.PMTime PmTime;
+
+            //当时间格式为xx:xx-yy:yy时
+            if (isExMatch(str.Replace(" ", ""), @"^(20|21|22|23|[0-1]?\d:[0-5]?\d)-(20|21|22|23|[0-1]?\d:[0-5]?\d)$", out MathGroup))
+            {
+                AmTime = new Form1.AMTime(Convert.ToDateTime(MathGroup[0]).TimeOfDay);
+                PmTime = new Form1.PMTime(Convert.ToDateTime(MathGroup[1]).TimeOfDay);
+                dateTime = new Form1.WorkTime(AmTime, PmTime);//(AmTime, PmTime);
+
+            }
+
+            //时间格式为xx:xx:xx时，只提取前4位数字
+            else if (isExMatch(str.Replace(" ", ""), @"^(20|21|22|23|[0-1]?\d:[0-5]?\d):[0-5]?\d$", out MathGroup))
+            {
+
+                //string temp = Convert.ToDateTime(MathGroup[0]).ToShortTimeString().ToString();
+                AmTime = new Form1.AMTime(Convert.ToDateTime(MathGroup[0]).TimeOfDay);//.ToShortTimeString()));
+                dateTime = new Form1.WorkTime(AmTime);
+                //listWorkTime.Add(_workTime);
+
+            }
+
+            //时间格式为xx:xx:xx 汉字（0-4位）时 //^[\u4e00-\u9fa5]{3}
+            else if (isExMatch(str.Replace(" ", ""), @"^(20|21|22|23|[0-1]?\d:[0-5]?\d):[0-5]?\d[\u4e00-\u9fa5]{0,4}$", out MathGroup))
+            {
+                //string temp = Convert.ToDateTime(MathGroup[0]).ToShortTimeString().ToString();
+                AmTime = new Form1.AMTime(Convert.ToDateTime(MathGroup[0]).TimeOfDay);//.ToShortTimeString()));
+                dateTime = new Form1.WorkTime(AmTime);
+                //listWorkTime.Add(_workTime);
+
+            }
+
+            //时间格式为xx:xx-
+            else if (isExMatch(str.Replace(" ", ""), @"^(20|21|22|23|[0-1]?\d:[0-5]?\d)-$", out MathGroup))
+            {
+                //dr[dc] = Convert.ToDateTime(MathGroup[0]).ToShortTimeString().ToString();
+                AmTime = new Form1.AMTime(Convert.ToDateTime(MathGroup[0]).TimeOfDay);//.ToShortTimeString()));
+                dateTime = new Form1.WorkTime(AmTime);
+                //listWorkTime.Add(_workTime);
+
+            }
+            //时间格式为-xx:xx
+            else if (isExMatch(str.Replace(" ", ""), @"^-(20|21|22|23|[0-1]?\d:[0-5]?\d)$", out MathGroup))
+            {
+                //dr[dc] = Convert.ToDateTime(MathGroup[0]).ToShortTimeString().ToString();
+                PmTime = new Form1.PMTime((Convert.ToDateTime(MathGroup[0])).TimeOfDay);//.ToShortTimeString()));
+                dateTime = new Form1.WorkTime(PmTime);
+                //listWorkTime.Add(_workTime);
+
+            }
+
+
+            return dateTime;
+        }
+
+        public bool isExMatch(string text, string patten, out List<string> Match)
+        {
+            bool _isMatch = false;
+            Regex Patten = new Regex(patten);
+            List<string> _match = new List<string>();
+            //if (Regex.IsMatch(text, patten))
+            if (Patten.Match(text).Success)
+            {
+                _isMatch = true;
+                for (int num = 1; num < Patten.Match(text).Groups.Count; num++)
+                {
+                    _match.Add(Patten.Match(text).Groups[num].Value);
+                }
+
+            }
+            else
+                _isMatch = false;
+            Match = _match;
+            return _isMatch;
+        }
+
+    }
+}
---
+++ C_Excel/WfSetting.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace C_Excel
+{
+    public partial class WfSetting : Form
+    {
+        public WfSetting()
+        {
+            InitializeComponent();
+        }
+        //public static DateTime LimitShowUpTime;// = Convert.ToDateTime("08:46:00");
+        //最早下班时间
+        //public static DateTime LimitDismissTime;// = Convert.ToDateTime("17:30:00");
+        public class WorkPlan
+        {
+            private DateTime _showUp;
+            public DateTime showUpAt
+            {
+                get { return _showUp; }
+                set { _showUp = value; }
+            }
+
+            private DateTime _dissmis;
+            public DateTime dissmisAt
+            {
+                get { return _dissmis; }
+                set { _dissmis = value; }
+            }
+
+            public WorkPlan()
+            {
+
+            }
+
+            public WorkPlan(DateTime ShowUpTime,DateTime DissmisTime)
+            {
+                this.showUpAt = ShowUpTime;
+                this.dissmisAt = DissmisTime;
+            }
+
+        }
+
+        XmlDocument xmldoc;
+        //XmlNode xmlnode;
+        //XmlElement xmlelem;
+
+        private void B_Valide_Click(object sender, EventArgs e)
+        {
+            if(T_ShowUp.Text!="" && T_Dissmis.Text != "")
+            {
+                try
+                {
+                    xmldoc = new XmlDocument();//@"..\..\Book.xml")
+                    xmldoc.Load(@"..\..\DataFile\WorkTime.xml");    //读取指定的XML文档
+                    XmlElement xe = xmldoc.DocumentElement;//获取xml文档的根xmlelement
+
+                    if (xe.HasChildNodes)
+                    {
+                        ///三种查询xml文件数据的方式
+                        /// 
+                        //string strPath = string.Format("workTime/ShowUpTime");
+                        //XmlElement selectXe = (XmlElement)xmldoc.SelectSingleNode(strPath);//selectSingleNode 根据XPath表达式,获得符合条件的第一个节点.
+                        ///
+                        //string a = selectXe.GetElementsByTagName("ShowUpTime")[0].InnerText;
+                        ///
+                        //XmlNodeList list = xmldoc.GetElementsByTagName("ShowUpTime");//要查询的的节点名  
+
+                        XmlNodeList listShowUpTime = xmldoc.GetElementsByTagName("ShowUpTime");//要查询的的节点名  
+                        XmlNodeList listDissmisTime = xmldoc.GetElementsByTagName("dissmisTime");//要查询的的节点名  
+
+                        string showUpT = listShowUpTime[0].InnerText.ToString();
+                        string dissmisT = listDissmisTime[0].InnerText.ToString();
+                                                                               //@"^(20|21|22|23|[0-1]?\d:[0-5]?\d)$"
+                        if (isExMatch(T_ShowUp.Text.ToString().Replace(" ", ""), @"(20|21|22|23|[0-1]?\d:[0-5]?\d)$")
+                            && isExMatch(T_Dissmis.Text.ToString().Replace(" ", ""), @"(20|21|22|23|[0-1]?\d:[0-5]?\d)$"))
+                        {
+                            if (showUpT != T_ShowUp.Text.ToString() || dissmisT != T_Dissmis.Text.ToString())
+                            {
+                                xmldoc.GetElementsByTagName("ShowUpTime")[0].InnerText = T_ShowUp.Text.ToString();
+                                xmldoc.GetElementsByTagName("dissmisTime")[0].InnerText = T_Dissmis.Text.ToString();
+                                xmldoc.Save(@"..\..\DataFile\WorkTime.xml");
+
+                                //LimitShowUpTime = Convert.ToDateTime(T_ShowUp.Text.ToString() + ":00");
+                                //LimitDismissTime = Convert.ToDateTime(T_Dissmis.Text.ToString() + ":00");
+
+                                
+                                //MessageBox.Show(""+ Form1._limitShowUpTime.ToShortTimeString());
+                                //传递值回form1
+                                ((Form1)this.Owner).SetLimShowUpTime = Convert.ToDateTime(T_ShowUp.Text.ToString());
+                                ((Form1)this.Owner).SetLimDissmisTime = Convert.ToDateTime(T_Dissmis.Text.ToString());
+                                MessageBox.Show("已保存", "通知");
+                            }
+                            else
+                            {
+                                MessageBox.Show("数据未改变", "通知");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("数据格式不正确", "通知");
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show("" + ex, "错误！");
+                }
+            }
+        }
+
+        private void B_Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WfSetting_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                xmldoc = new XmlDocument();
+                xmldoc.Load(@"..\..\DataFile\WorkTime.xml");    //读取指定的XML文档
+                XmlNode NodeWorkTime = xmldoc.DocumentElement;  //读取xml的根节点
+
+                foreach (XmlNode node in NodeWorkTime.ChildNodes)//循环子节点
+                {
+                    switch (node.Name)
+                    {
+                        case "ShowUpTime":
+                            if (node.InnerText != "")
+                            {
+                                T_ShowUp.Text = node.InnerText;
+                            }
+
+                            break;
+
+                        case "dissmisTime":
+                            if (node.InnerText != "")
+                            {
+                                T_Dissmis.Text = node.InnerText;
+                            }
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {                
+                MessageBox.Show("" + ex, "错误");
+            }
+        }
+
+        public bool isExMatch(string text, string patten)
+        {
+            bool _isMatch = false;
+            Regex Patten = new Regex(patten);
+            List<string> _match = new List<string>();
+            //if (Regex.IsMatch(text, patten))
+            if (Patten.Match(text).Success)
+            {
+                _isMatch = true;
+                for (int num = 1; num < Patten.Match(text).Groups.Count; num++)
+                {
+                    _match.Add(Patten.Match(text).Groups[num].Value);
+                }
+
+            }
+            else
+            {
+                _isMatch = false;
+            }
+
+            return _isMatch;
+        }
+    }
+}
